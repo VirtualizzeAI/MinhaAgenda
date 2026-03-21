ac0b9ae42c4a17979ef5074cf2926c96f37c0528
VirtualizzeAI <virtualizzeautomacao@gmail.com>
2026-03-21 19:55:42 -0300

    fix: tradução das datas

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client';
 import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css';
 import './styles/global.css';
+import dayjs from 'dayjs';
+import 'dayjs/locale/pt-br';
 import App from './App';
+
+dayjs.locale('pt-br');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
